937b364f329567e75c310b6ef128ffbde8a3ea29
shreshthipatil90 <shreshthipatil90@gmail.com>
2026-04-05 17:46:41 +0530

    Update test_classification.py -Shreshthi

--- test_classification.py
+++ test_classification.py
@@ -1,9 +1,12 @@
+
 from classification import classify_company
 
 test_data = [
     {"manual_roles": 6, "manual_keywords": 4, "scaling": True},
     {"manual_roles": 1, "manual_keywords": 0, "scaling": False},
     {"manual_roles": 3, "manual_keywords": 2, "scaling": False},
+    {"manual_roles": 0, "manual_keywords": 1, "scaling": False},
+    {"manual_roles": 5, "manual_keywords": 3, "scaling": False},
 ]
 
 for i, company in enumerate(test_data):
